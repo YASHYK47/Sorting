4949e410ee9e70cc67cd052bb87e36c7f156cc94
Yash Kesarwani <yashky47@gmail.com>
2020-10-02 02:08:40 +0530

    Header File and Indentation

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,28 +1,26 @@
-#include<stdio.h>
-#include<conio.h>
+#include<bits/stdc++.h>
 int main()
 {
-	int i,hole,A[100],value=0,n;
-	printf("Enter number of elements: ");
-	scanf("%d",&n);
-	printf("\nEnter the elements:\n");
-	for(i=0;i<n;i++)
-	scanf("%d",&A[i]);
-	for(i=1;i<n;i++)
+	int i, hole, A[100], value=0, n;
+	printf( "Enter number of elements: ");
+	scanf( "%d", &n);
+	printf( "\nEnter the elements:\n" );
+	for( i=0; i<n; i++)
+	scanf( "%d", &A[i] );
+	for( i=1; i<n; i++)
 	{
-		value=A[i];
-		hole=i;
-		while(hole>0 && A[hole-1]>value)
+		value = A[i];
+		hole = i;
+		while( hole > 0 && A[hole-1] > value )
 		{
-			A[hole]=A[hole-1];
+			A[hole] = A[hole-1];
 			hole--;
 		}
-		A[hole]=value;
+		A[hole] = value;
 	}
-	printf("\nSorted Array is:\n");
-	for(i=0;i<n;i++)
-	printf("%d\n",A[i]);
-	
+	printf( "\nSorted Array is:\n" );
+	for( i=0; i<n; i++)
+	printf( "%d\n", A[i] );
 	return 0;
 }
 //Best case: O(n)
